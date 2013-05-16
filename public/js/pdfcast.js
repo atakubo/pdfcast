c9db6caa4687704beba1e3058105e4857258492e
A. Takubo <atakubo4o@gmail.com>
2013-05-17 02:27:28 +0900

    add paging control

--- public/js/pdfcast.js
+++ public/js/pdfcast.js
@@ -3,21 +3,34 @@
 
 // display PDF file
 
+var target = "pdf_canvas";
+var pdfDoc = null;
+var curPage = 1;
+
 PDFJS.disableWorker = true;
 //
 // Fetch the PDF document from the URL using promices
 //
 function loadPdf(url) {
   PDFJS.getDocument(url).then(function(pdf) {
+    pdfDoc = pdf;
+    curPage = 1;
+    bindPaging();
+    renderPdf(curPage);
+  });
+}
+
+function renderPdf(pageNum) {
+    $("#page_info").text(pageNum.toString() + " / " + pdfDoc.numPages.toString());
     // Using promise to fetch the page
-    pdf.getPage(1).then(function(page) {
+    pdfDoc.getPage(pageNum).then(function(page) {
       var scale = 1.5;
       var viewport = page.getViewport(scale);
 
       //
       // Prepare canvas using PDF page dimensions
       //
-      var canvas = document.getElementById('pdf_canvas');
+      var canvas = document.getElementById(target);
       var context = canvas.getContext('2d');
       canvas.height = viewport.height;
       canvas.width = viewport.width;
@@ -31,8 +44,26 @@ function loadPdf(url) {
       };
       page.render(renderContext);
     });
-  });
 }
+
+function next() {
+  if(!pdfDoc || curPage == pdfDoc.numPages) return;
+  renderPdf(++curPage);
+}
+
+function prev() {
+  if(!pdfDoc || curPage == 1) return;
+  renderPdf(--curPage);
+} 
+
+function bindPaging() {
+  $("#pdf_canvas").on("click", next);
+};
+
+function unbindPaging() {
+
+};
+
 
 
 // websocket 
@@ -43,3 +74,5 @@ function loadPdf(url) {
 function copy() {
   $("#copy_img").attr("src", $("#pdf_canvas")[0].toDataURL());
 }
+
+bindPaging();
