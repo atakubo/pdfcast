176095918a836ca99262f54d901554aa7f5711ae
A. Takubo <atakubo4o@gmail.com>
2013-05-21 08:18:40 +0900

    add watch page to thumbnails
    * thumbnails with bxSlider
    * style changing

--- public/js/watch.js
+++ public/js/watch.js
@@ -1,4 +1,16 @@
 
+var slider;
+var imgArray = [];
+var option = {
+  infiniteLoop: false, 
+  hideControlOnEnd: true, 
+  pager: false,
+  minSlides: 3, 
+  maxSlides: 5,
+  slideMargin: 10, 
+  slideWidth: 200, 
+};
+
 $(document).ready(function() {
   // initializing socket.io
   var socket = io.connect("/");
@@ -10,10 +22,27 @@ $(document).ready(function() {
 
 // receiving new page
 function receivePage(data) {
-  $("#page").attr("src", data["image"]);
+  $("#main_img").attr("src", data["image"]);
+  addList(data);
+  reloadSlider();
 }
 
-function addPage() {
+function addList(data) {
+  $("#list").append(
+    $("<li>").attr("id", data["name"]).append(
+      $("<img>").attr("id", "img_" + data["name"])
+                .attr("class", "page")
+                .attr("src", data["image"])
+                .attr("width", 200)
+    )
+  );
+}
 
+function reloadSlider() {
+  if (!slider) {
+    slider = $(".bxslider").bxSlider(option);
+  } else {
+    slider.reloadSlider(option);
+  }
 }
 
