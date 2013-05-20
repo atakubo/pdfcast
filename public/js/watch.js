1139c73538a35ddc414db29bc8a7889134598c88
A. Takubo <atakubo4o@gmail.com>
2013-05-20 23:05:12 +0900

    code clean up

--- public/js/watch.js
+++ public/js/watch.js
@@ -1,9 +1,19 @@
-var socket = io.connect("/");
-socket.on("page", function(data) {
-  receivePage(data);
+
+$(document).ready(function() {
+  // initializing socket.io
+  var socket = io.connect("/");
+  socket.on("page", function(data) {
+    receivePage(data);
+  });
+
 });
 
+// receiving new page
 function receivePage(data) {
   $("#page").attr("src", data["image"]);
+}
+
+function addPage() {
+
 }
 
